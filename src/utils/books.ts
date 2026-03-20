ba79508c9924258dc3b343e2772c50d5ad752e24
Yusuke Ohmori <yusuke_ohmori_acn@maas.jp.honda>
2026-03-21 04:27:45 +0900

    feat: 画像を差し替え

---
+++ src/utils/books.ts
@@ -0,0 +1,10 @@
+export const books = [
+  {
+    url: 'https://engineers-anime.booth.pm/items/7666750',
+    id: '7666750',
+  },
+  {
+    url: 'https://engineers-anime.booth.pm/items/7666770',
+    id: '7666770',
+  },
+]
