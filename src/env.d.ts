4dc3a93146457f578057e2005c7d65d9da0ac707
Yusuke Ohmori <yusuke_ohmori_acn@maas.jp.honda>
2026-03-21 04:31:43 +0900

    fix: 拡張子を修正

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '*.JPG' {
+  const content: import('astro').ImageMetadata
+  export default content
+}
